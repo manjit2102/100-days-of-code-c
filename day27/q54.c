db5507d460b1d3e293186795e51c1b819d7c15cb
manjit2102 <manjit.41123@stu.upes.ac.in>
2026-09-09 09:35:20 +0530

    day27

---
+++ day27/q54.c
@@ -0,0 +1,55 @@
+/*
+*name:manjit
+*roll:590041123
+*day:27 question:2
+*date:05-09-2026
+*
+*problem statement:
+*write a program to print the following pattern:
+*   *
+*  ***
+* *****
+********
+* *****
+*  ***
+*   *
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 4; i++)
+    {
+        for(j = 1; j <= 4 - i; j++)
+        {
+            printf(" ");
+        }
+
+        for(j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for(i = 3; i >= 1; i--)
+    {
+        for(j = 1; j <= 4 - i; j++)
+        {
+            printf(" ");
+        }
+
+        for(j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
