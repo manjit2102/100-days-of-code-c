324bbb2093e02c270bcce682a9fefc29fae06e77
manjit2102 <manjit.41123@stu.upes.ac.in>
2026-09-13 16:34:23 +0530

    day32

---
+++ day32/q63.c
@@ -0,0 +1,48 @@
+/*
+*name:manjit
+*roll:590041123
+*day:32 question:1
+*date:10-09-2026
+*
+*problem statement:
+*Merge two arrays.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, a[100], b[100], c[200];
+    int i;
+
+    scanf("%d", &n1);
+
+    for(i=0; i<n1; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &n2);
+
+    for(i=0; i<n2; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    for(i=0; i<n1; i++)
+    {
+        c[i]=a[i];
+    }
+
+    for(i=0; i<n2; i++)
+    {
+        c[n1+i]=b[i];
+    }
+
+    for(i=0; i<n1+n2; i++)
+    {
+        printf("%d ", c[i]);
+    }
+
+    return 0;
+}
