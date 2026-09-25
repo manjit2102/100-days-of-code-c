5acfa1f4d1631c439dc6b0c85107403f57c824e0
manjit2102 <manjit.41123@stu.upes.ac.in>
2026-09-25 17:20:45 +0530

    day01

---
+++ day01/q1.c
@@ -0,0 +1,23 @@
+/*
+name:manjit
+roll:590041123
+day:1 question:1
+date:10-08-2026
+
+Q1: Write a program to input two numbers and display their sum.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
---
+++ day01/q2.c
@@ -0,0 +1,22 @@
+/*
+name:manjit
+roll:590041123
+day:1 question:2
+date:10-08-2026
+
+Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d",
+           a + b, a - b, a * b, a / b);
+
+    return 0;
+}
